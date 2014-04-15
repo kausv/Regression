32cdc91bc637d39b59f6a2f7694c5af31eef33a3
Kaustubh <kausvartak@gmail.com>
2014-04-15 13:00:33 -0400

    Starting gradient decent
    Need to start with gradient descent

--- base code/script.m
+++ base code/script.m
@@ -37,59 +37,59 @@ figure;
 plot([train_errors test_errors]);
 legend('Training Error','Testing Error');
 %%% END PROBLEM 2 CODE %%%
-% 
-% %%% BEGIN PROBLEM 3 CODE
-% % ridge regression using gradient descent - see handouts (lecture 21 p5) or
-% % http://cs229.stanford.edu/notes/cs229-notes1.pdf (page 11)
-% initialWeights = zeros(65,1);
-% % set the maximum number of iteration in conjugate gradient descent
-% options = optimset('MaxIter', 500);
-% 
-% % define the objective function
-% lambdas = 0:0.00001:0.001;
-% train_errors = zeros(length(lambdas),1);
-% test_errors = zeros(length(lambdas),1);
-% 
-% % run ridge regression training with fmincg
-% for i = 1:length(lambdas)
-%     lambda = lambdas(i);
-%     objFunction = @(params) regressionObjVal(params, x_train_i, y_train, lambda);
-%     w = fmincg(objFunction, initialWeights, options);
-%     % fill code here for prediction and computing errors
-% end
-% figure;
-% plot([train_errors test_errors]);
-% legend('Training Error','Testing Error');
-% %%% END PROBLEM 3 CODE
-% 
-% %%% BEGIN  PROBLEM 4 CODE
-% % using variable number 3 only
-% x_train = x_train(:,3);
-% x_test = x_test(:,3);
-% train_errors = zeros(7,1);
-% test_errors = zeros(7,1);
-% 
-% % no regularization
-% lambda = 0;
-% for d = 0:6
-%     x_train_n = mapNonLinear(x_train,d);
-%     x_test_n = mapNonLinear(x_test,d);
-%     w = learnRidgeRegression(x_train_n,y,lambda);
-%     % fill code here for prediction and computing errors
-% end
-% figure;
-% plot([train_errors test_errors]);
-% legend('Training Error','Testing Error');
-% 
-% % optimal regularization
-% lambda = lambda_optimal; % from part 2
-% for d = 0:6
-%     x_train_n = mapNonLinear(x_train,d);
-%     x_test_n = mapNonLinear(x_test,d);
-%     w = learnRidgeRegression(x_train_n,y,lambda);
-%     % fill code here for prediction and computing errors
-% end
-% figure;
-% plot([train_errors test_errors]);
-% legend('Training Error','Testing Error');
+
+%%% BEGIN PROBLEM 3 CODE
+% ridge regression using gradient descent - see handouts (lecture 21 p5) or
+% http://cs229.stanford.edu/notes/cs229-notes1.pdf (page 11)
+initialWeights = zeros(65,1);
+% set the maximum number of iteration in conjugate gradient descent
+options = optimset('MaxIter', 500);
+
+% define the objective function
+lambdas = 0:0.00001:0.001;
+train_errors = zeros(length(lambdas),1);
+test_errors = zeros(length(lambdas),1);
+
+% run ridge regression training with fmincg
+for i = 1:length(lambdas)
+    lambda = lambdas(i);
+    objFunction = @(params) regressionObjVal(params, x_train_i, y_train, lambda);
+    w = fmincg(objFunction, initialWeights, options);
+    % fill code here for prediction and computing errors
+end
+figure;
+plot([train_errors test_errors]);
+legend('Training Error','Testing Error');
+%%% END PROBLEM 3 CODE
+
+%%% BEGIN  PROBLEM 4 CODE
+% using variable number 3 only
+x_train = x_train(:,3);
+x_test = x_test(:,3);
+train_errors = zeros(7,1);
+test_errors = zeros(7,1);
+
+% no regularization
+lambda = 0;
+for d = 0:6
+    x_train_n = mapNonLinear(x_train,d);
+    x_test_n = mapNonLinear(x_test,d);
+    w = learnRidgeRegression(x_train_n,y,lambda);
+    % fill code here for prediction and computing errors
+end
+figure;
+plot([train_errors test_errors]);
+legend('Training Error','Testing Error');
+
+% optimal regularization
+lambda = lambda_optimal; % from part 2
+for d = 0:6
+    x_train_n = mapNonLinear(x_train,d);
+    x_test_n = mapNonLinear(x_test,d);
+    w = learnRidgeRegression(x_train_n,y,lambda);
+    % fill code here for prediction and computing errors
+end
+figure;
+plot([train_errors test_errors]);
+legend('Training Error','Testing Error');
 
